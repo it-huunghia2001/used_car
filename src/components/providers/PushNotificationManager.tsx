96c30084033169ed2ba561bc18acd5fdde10e374
Hoang Huu Nghia <hoanghuunghia.dev@gmail.com>
2026-03-07 17:04:00 +0700

    fix all

--- src/components/providers/PushNotificationManager.tsx
+++ src/components/providers/PushNotificationManager.tsx
@@ -19,6 +19,7 @@ export default function PushNotificationManager({
             appId: "60633c73-3211-4b9c-a80c-efe41d56df17", // App ID thật của Nghĩa
             allowLocalhostAsSecureOrigin: true,
             serviceWorkerPath: "/OneSignalSDKWorker.js",
+            siteStack: "https://www.xecubinhduong.com.vn",
           });
 
           // Liên kết User ID từ hệ thống của bạn với OneSignal
